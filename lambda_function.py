fd028256eedf924b2390d17cf34ce8f3619752b1
aja-min <n.a.goominmin5577@gmail.com>
2024-06-01 16:30:42 +0900

    first commit

---
+++ lambda_function.py
@@ -0,0 +1,75 @@
+import os
+import sys
+import logging
+
+from linebot import (LineBotApi, WebhookHandler)
+from linebot.models import (MessageEvent, TextMessage, TextSendMessage)
+from linebot.exceptions import (LineBotApiError, InvalidSignatureError)
+
+logger = logging.getLogger()
+logger.setLevel(logging.ERROR)
+
+#LINEBOTと接続するための記述
+#環境変数からLINEBotのチャンネルアクセストークンとシークレットを読み込む
+channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+
+#無いならエラー
+if channel_secret is None:
+    logger.error('Specify LINE_CHANNEL_SECRET as environment variable.')
+    # sys.exit(1)
+if channel_access_token is None:
+    logger.error('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+    # sys.exit(1)
+
+#apiとhandlerの生成（チャンネルアクセストークンとシークレットを渡す）
+line_bot_api = LineBotApi(channel_access_token)
+handler = WebhookHandler(channel_secret)
+
+#Lambdaのメインの動作
+def lambda_handler(event, context):
+    print("Event:", event)
+    # 認証用のx-line-signatureヘッダー
+    if "headers" in event and "x-line-signature" in event["headers"]:
+        signature = event["headers"]["x-line-signature"]
+    else:
+        # エラーレスポンスを返す
+        return {
+            "isBase64Encoded": False,
+            "statusCode": 400,
+            "headers": {},
+            "body": "Missing x-line-signature header"
+        }
+    body = event["body"]
+    # リターン値の設定
+    ok_json = {
+        "isBase64Encoded": False,
+        "statusCode": 200,
+        "headers": {},
+        "body": ""
+    }
+    error_json = {
+        "isBase64Encoded": False,
+        "statusCode": 500,
+        "headers": {},
+        "body": "Error"
+    }
+
+    #メッセージを受け取る・受け取ったら受け取ったテキストを返信する
+    @handler.add(MessageEvent, message=TextMessage)
+    def message(line_event):
+        text = line_event.message.text
+        line_bot_api.reply_message(line_event.reply_token, TextSendMessage(text=text))
+
+    #例外処理としての動作
+    try:
+        handler.handle(body, signature)
+    except LineBotApiError as e:
+        logger.error("Got exception from LINE Messaging API: %s\n" % e.message)
+        for m in e.error.details:
+            logger.error("  %s: %s" % (m.property, m.message))
+        return error_json
+    except InvalidSignatureError:
+        return error_json
+
+    return ok_json
