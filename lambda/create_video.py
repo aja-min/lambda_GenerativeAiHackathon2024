ef4778418b122ea34ced8e0e6db0805eaf8c02b4
Hachi8w8 <162113508+Hachi8w8@users.noreply.github.com>
2024-06-02 13:24:55 +0900

    ログ追加

--- lambda/create_video.py
+++ lambda/create_video.py
@@ -132,6 +132,7 @@ def create_presigned_url(bucket_name, object_name, expiration=3600):
     """Generate a presigned URL to share an S3 object"""
     s3_client = boto3.client('s3')
     try:
+        print("try s3_client.generate_presigned_url")
         response = s3_client.generate_presigned_url('get_object',
                                                     Params={'Bucket': bucket_name,
                                                             'Key': object_name},
